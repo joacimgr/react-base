e464b74050a78b8cf6c7a719347f794a49a2dd7f
Joacim Granberg <joacim.granberg@outlook.com>
2020-09-07 22:45:10 +0200

    remove build from .gitignore

---
+++ build/precache-manifest.1cd1e6e12452ad506317f0681773dbb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578256d865177ea28fff8325dc038b05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e250871462a121516a65",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5be633fe267579d39b76",
+    "url": "/static/js/2.c939a70c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c939a70c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e250871462a121516a65",
+    "url": "/static/js/main.115de5f3.chunk.js"
+  },
+  {
+    "revision": "d444a593c3da1a3b8a1b",
+    "url": "/static/js/runtime-main.a9edd29d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

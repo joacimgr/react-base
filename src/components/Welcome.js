c3fa22bfe3f07521e93f516713285f04fa2e9074
joacim <joacimgr@kth.se>
2020-11-15 13:17:27 +0100

    remove ip test

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,38 +2,11 @@ import React from 'react';
 import publicIp from 'react-public-ip';
 
 class Welcome extends React.Component {
-    constructor(props){
-        super(props);
-        var valueIP = "";
-    }
-
-    componentDidMount(){
-        this.forceUpdate();
-    }
-    
-    async getIp(){
-        var ipv4 = await publicIp.v4() || "";
-        console.log("ipv4 = ", ipv4);
-        return ipv4;
-    }
-    
+ 
     render() {
-        try{
-            this.getIp().then(ip => {
-                this.value = ip;
-                console.log("inside getip: ", "value: ", this.value, "ip: ", ip);
-                if(this.value != null){
-                    this.forceUpdate();
-                }
-            })
-        } catch (err){
-            console.error(err);
-        }
          
-        console.log("value: ", this.value, "ip: ", this.value)
         return <div>
             <p>Hello, {this.props.name}</p>
-            <p>your IP is: {this.value}</p> 
             </div>;
         }
 

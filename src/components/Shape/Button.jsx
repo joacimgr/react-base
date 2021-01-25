d7c536962088b486bbaa5db5833593eccf761408
Joacim Granberg <joacim.granberg@outlook.com>
2021-01-25 08:35:24 +0100

    merge

---
+++ src/components/Shape/Button.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import '../../Styles/Shape.scss';
+
+export default class Button extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: "white",
+            width: 1001,
+            height: 1001,
+            array: [1,2,3,4],
+        };
+    }
+
+    
+
+    onHover(event){
+        console.log("onHover!!");
+        console.log("X: ", event.clientX);
+        console.log("Y : ", event.clientY);
+        //this.setState((state, props) => { return { array: [2,2,2,2] }})
+    }
+
+    render() {
+        var element = <div className="shape" onMouseEnter={this.onHover}>{this.state.array}</div>
+        return (
+            <div>
+                {element}
+            </div>
+        );
+    }
+}
